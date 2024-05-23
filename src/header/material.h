cbd6d109be2139dc6950deab32e40e34a98b5d0e
Priyanshu-CODERX <priyanshubh999@gmail.com>
2024-05-23 12:54:31 +0530

    feat: added schlicks approximation for accurate angled reflection on dielectric material

--- src/header/material.h
+++ src/header/material.h
@@ -74,7 +74,7 @@ class dieletric : public material
             bool cannot_refract = ri*sin_theta > 0;
             vec3 direction;
 
-            if(cannot_refract)
+            if(cannot_refract || reflectance(cos_theta, ri) > random_double())
                 direction = reflect(unit_direction, rec.normal);
             else
                 direction = refract(unit_direction, rec.normal, ri);
@@ -85,6 +85,14 @@ class dieletric : public material
 
     private:
         double refraction_index;
+
+        static double reflectance(double cosine, double refraction_index)
+        {
+            // using schlicks approximation for reflectance
+            auto r0 = (1-refraction_index) / (1+refraction_index);
+            r0 = r0 * r0;
+            return r0+(1-r0)*pow((1-cosine), 5);
+        }
 };
 
 #endif
